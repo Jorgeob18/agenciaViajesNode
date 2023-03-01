a3496d1cc1f38d9f61e505b19ea63e5fdb2e92d4
Jorgeob18 <jorgealbertoob18@gmail.com>
2023-03-01 13:58:07 -0600

    Hciendo Routin en Express

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  // req: lo que enviamos, res: lo que recibimos
+  res.send('Inicio');
+});
+
+router.get('/nosotros', (req, res) => {
+  res.send('Nosotros');
+});
+
+router.get('/contacto', (req, res) => {
+  res.send('Contacto');
+});
+
+export default router;
