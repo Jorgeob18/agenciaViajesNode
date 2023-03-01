a055aef81e125637d711000577c0e9179340471d
Jorgeob18 <jorgealbertoob18@gmail.com>
2023-03-01 12:57:15 -0600

    Habilitando sintaxis de Import y Export

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const express = require('express');
+// const express = require('express');
+
+import express from 'express'; // Se agrega "type": "module", en package.json
 
 const app = express();
 
 // Definir puertp
 const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  // req: lo que enviamos, res: lo que recibimos
+  res.send('Inicio');
+});
+
+app.get('/nosotros', (req, res) => {
+  // req: lo que enviamos, res: lo que recibimos
+  res.send('Nosotros');
+});
+
+app.get('/contacto', (req, res) => {
+  // req: lo que enviamos, res: lo que recibimos
+  res.send('Contacto');
+});
 
 app.listen(port, () => {
   console.log(`El servidor esta funcionando en el puerto ${port}`);
