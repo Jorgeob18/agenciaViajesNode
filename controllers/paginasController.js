a32c21b7682c7bd195aa4e48ae39adc782eb2ffe
Jorgeob18 <jorgealbertoob18@gmail.com>
2023-03-15 16:49:48 -0600

    Realizar varias consultas, agrupar async await - Promise.all

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -3,14 +3,20 @@ import { Testimonial } from '../models/Testimoniales.js';
 
 const paginaInicio = async (req, res) => {
   // Consultar 3 viajes del modelo Viaje
+  // Correccion branch test
+  //Ejecutar al mismo tiempo las dos consultas en lugar de hacer await a una y despues a otra
+  const promiseDB = [];
+
+  promiseDB.push(Viaje.findAll({ limit: 3 }));
+  promiseDB.push(Testimonial.findAll({ limit: 3 }));
+
   try {
-    const viajes = await Viaje.findAll({ limit: 3 });
-    const testimoniales = await Testimonial.findAll({ limit: 3 });
+    const resultado = await Promise.all(promiseDB);
     res.render('inicio', {
       pagina: 'Home',
       clase: 'home',
-      viajes,
-      testimoniales,
+      viajes: resultado[0],
+      testimoniales: resultado[1],
     });
   } catch (error) {
     console.log(error);
